8243042bf0c7efefe44d9ed066c51cc296868d14
sunilthornwork-web <sunilthorn.work@gmail.com>
2026-09-06 20:19:29 +0700

    Add files via upload

---
+++ config.production.js
@@ -0,0 +1,5 @@
+window.LUOPANICH_CONFIG = {
+  ENVIRONMENT: "PRODUCTION",
+  EXPECTED_ENVIRONMENT: "PRODUCTION",
+  API_BASE_URL: "https://script.google.com/macros/s/AKfycbxO5fb9-rzRAeuoogjdaBU8LB42jk46GmKNrv8H6XjTaRAcN2PbOr35YkZ36y2cti_Fkw/exec"
+};
